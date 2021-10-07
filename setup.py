52480e95ffa9aa3250f362ed60154e5ad68d71a6
“[Iman <[iman.milo@yahoo.com]>
2021-10-07 14:12:14 +0200

    added new file

---
+++ setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name = 'fizzbuzz',
+    version = '1.0.0',
+    url = 'https://github.com/vous/fizzbuzz.git',
+    author = 'Vous',
+    author_email = 'vous@etu.unistra.fr',
+    description = 'Réalisation du TP 02',
+    packages = find_packages(),
+)
